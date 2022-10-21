95a73eb6fe7811e57352d64150d047e298ef700c
Magnus Lindholm Nielsen <malini@aarhus.dk>
2022-10-21 11:22:50 +0200

    minor fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,9 @@ import pytest  # type: ignore
 @pytest.fixture()
 def poetry_version() -> str:
     """
-    Fixture for getting a string representation of the output of the 'poetry version' command (not the 'poetry --version' command)
+    Fixture for getting a string representation of the output of the 'poetry version --short' command (Note: not the 'poetry --version' command)
     """
     poetry_process: subprocess.CompletedProcess[bytes] = subprocess.run(
-        ["poetry", "version"], stdout=subprocess.PIPE
+        ["poetry", "version", "--short"], stdout=subprocess.PIPE
     )
     return str(poetry_process.stdout)
