7e6dff9090ec75b7da6a2e53e546e9dcd097d379
Magnus Lindholm Nielsen <malini@aarhus.dk>
2022-11-04 14:15:50 +0100

    test

--- acautils/standard_log.py
+++ acautils/standard_log.py
@@ -7,7 +7,7 @@ def setup_logger(log_path: Path) -> logging.Logger:
     General method for setting op a log object. Ensures that the different logs we use across tools are
     standardized
     ## Args
-    * path: the path directly to the log´as a `txt` file
+    * path: the path directly to the log as a `txt` file
     """
     log: logging.Logger = logging.getLogger(log_path.name)
     file_handler: logging.FileHandler = logging.FileHandler(
