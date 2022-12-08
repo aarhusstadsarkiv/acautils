6972239c34929e3f304d15ee9ac6902c6d99e419
Magnus Lindholm Nielsen <malini@aarhus.dk>
2022-12-08 14:02:46 +0100

    Logger now creates the full path to the log given, also all intermediate directories

--- acautils/standard_log.py
+++ acautils/standard_log.py
@@ -10,8 +10,10 @@ def setup_logger(log_name: str, log_path: Path) -> logging.Logger:
     * log_path: the path directly to the log as a `txt` file. If the file is not there, it will be created. If it already exists, it will append the messages to the file.
     * log_name: the name given to the logger within the logging modules own namespace. All descendant logs needs to have a name on the form 'log_name.descendant_log_name', which often is the name of the module or submodule that the function is called from.
     """
+    # If the parents of the file does not exist, then we make them
     if not log_path.parent.exists():
-        Path.mkdir(log_path.parent)
+        Path.mkdir(log_path.parent, parents=True, exist_ok=True)
+
     log: logging.Logger = logging.getLogger(log_name)
     file_handler: logging.FileHandler = logging.FileHandler(
         log_path, "a", encoding="utf-8"
